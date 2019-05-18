231ea43ccc4dbdccb8831961b03bdd8ab00a992c
takata <takata@codelight.co.jp>
2019-05-18 14:33:44 +0900

    first commit

---
+++ Assets/Scripts/CdioTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CdioCs;
+
+public class CdioTest : MonoBehaviour
+{
+    private Cdio _cdio;
+    private short _id;
+
+    [SerializeField] private GameObject[] _obj;
+
+    void Start()
+    {
+        _cdio = new Cdio();
+        _cdio.Init("DIO000", out _id);
+
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            var data = new byte[2];
+            var bitNo = new short[] { 0, 1 };
+            _cdio.InpMultiBit(_id, bitNo, 2, data);
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (data[i] == 1)
+                {
+                    var r = Random.Range(1f, 3f);
+                    var vec = Random.onUnitSphere;
+                    var obj = GameObject.Instantiate(_obj[i], vec * r, Quaternion.identity);
+                    obj.transform.localScale = Vector3.one * r;
+                    obj.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0, 1, 0.8f, 0.8f, 0.8f, 0.8f);
+
+                    var rd = obj.GetComponent<Rigidbody>();
+                    rd.AddForce((Vector3.up + vec) * 10f, ForceMode.Impulse);
+                    rd.AddTorque(Random.onUnitSphere * 10f, ForceMode.Impulse);
+
+                    GameObject.Destroy(obj, 5f);
+                    yield return new WaitForSeconds(0.05f);
+                }
+            }
+            yield return null;
+        }
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnDestroy()
+    {
+        _cdio.Exit(_id);
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# CGWORLD連載サンプル
+CONTEC DIO-0808LY-USBを利用してUnityにボタンをつなげるサンプルです。
+
+ボタン0を押すと移動、ボタン1を押すと回転が早くなります。
+
+同時に押すと全体の移動量も増えます。
+
+オブジェクトの動きの制御に https://github.com/keijiro/Klak の Brownian Motion を利用しています。
+
+![DSC04972](https://user-images.githubusercontent.com/529150/57574779-44c36900-747a-11e9-837d-910726a72b93.jpg)
+
